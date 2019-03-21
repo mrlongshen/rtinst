c060ac11fb7b581d6e057e633ed00df42f2cf5df
Grip Reaper <mrlongshen@gmail.com>
2019-03-22 01:20:38 +0800

    Delete plugins

--- plugins/quotaspace/done.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$req = new rXMLRPCRequest( array(
-	$theSettings->getOnResumedCommand(array('quotapauser'.getUser(), getCmd('cat='))),
-	new rXMLRPCCommand("schedule_remove", 'quotaspace'.getUser())));
-$req->run();
-
-?>
--- plugins/quotaspace/lang/vi.js
+++
@@ -1,5 +0,0 @@
-﻿
- 
-theUILang.quotaWarn			= "<font color=red>Attention! The filesystem quota has been met.</font><ul><li>All downloads has been paused<li>The global download speed has been limited to 1KB/s</ul>To resume normal operation please:<ul><li>Free up some space<li>Start paused downloads manually</ul>";
-
-thePlugins.get("quotaspace").langLoaded();
